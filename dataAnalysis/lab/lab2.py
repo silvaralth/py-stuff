ee12281767c4c53d3a399c759334d817aaf90799
Sergio Ballen <silvaralth@gmail.com>
2020-07-25 04:31:57 -0500

    Updating labs

---
+++ dataAnalysis/lab/lab2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+filename = "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+
+df = pd.read_csv(filename, names = headers)
+
+df.replace("?", np.nan, inplace = True)
+mean = df["stroke"].astype('float').mean(axis=0)
+
+#df.head(5)
+
+df.replace(np.nan, mean, inplace = True)
+
